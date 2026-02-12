6a799551b2cf9ea74e7ff1d6d0a82aa744063a11
Khangne <dhkhang18112000@gmail.com>
2026-02-12 15:57:48 +0700

    luyen tap cai dat cay tim kiem nhi phan voi thao tac chen va duyet theo thu tu inorder

---
+++ thuchanh4/AbstractTree.java
@@ -0,0 +1,20 @@
+package thuchanh4;
+
+
+
+public class AbstractTree <E> implements Tree<E> {
+    @Override
+    public boolean insert(E e) {
+        return false;
+    }
+
+    @Override
+    public void inorder() {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+}
---
+++ thuchanh4/Tree.java
@@ -0,0 +1,7 @@
+package thuchanh4;
+
+public interface Tree <E>{
+    boolean insert(E e);
+    void inorder();
+    int getSize();
+}
